9be3fc2eaa4f0a18c86efb3e9c4d0eb7876e5ff5
Nahom_d54 <nahom.d54@gmail.com>
2024-03-10 01:09:47 +0300

    feat: Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+import Header from './components/Header';
+import Table from './components/Table';
+
+
+function App() {
+ 
+  return (
+    <>
+      {/* there should be two sides 
+       one side is navigation and the other is music list
+       */}
+    <Header />
+    <Table/>
+
+    </>
+  )
+}
+
+export default App
