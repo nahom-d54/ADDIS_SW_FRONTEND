ea65091444cc14c0d4018acac787de10502fdfc7
Nahom_d54 <nahom.d54@gmail.com>
2024-03-13 14:36:35 +0300

    mod: modified rootsaga to include user saga

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,9 +1,11 @@
 import { all } from "redux-saga/effects";
 import { watchMusicAsync } from "./musicSaga";
+import { watchUserAsync } from "./userSaga";
 
 
 export default function* rootSaga(){
     yield all([
-        watchMusicAsync()
+        watchMusicAsync(),
+        watchUserAsync()
     ])
 }
