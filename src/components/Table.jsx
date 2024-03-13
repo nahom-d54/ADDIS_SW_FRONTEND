af84a293d31f427a71497315c5d6e489711cfba9
Nahom_d54 <nahom.d54@gmail.com>
2024-03-13 14:35:27 +0300

    feat: added hideing feature until enough data is on page for pagination

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -7,16 +7,27 @@ import { setMusicSlice } from "../redux/slices/music";
 import Editor from "./Editor";
 import Pagination from "./Pagination";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faClockRotateLeft, faMusic, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faClockRotateLeft, faDownload, faMusic, faUser } from "@fortawesome/free-solid-svg-icons";
 import Skeleton from "./Skeleton";
 import { isLoading } from "../redux/slices/musics";
+
 
 
 export default function Table() {
     const dispatch = useDispatch();
     const musicList = useSelector(state => state.musics)
-
+    const user = useSelector(state => state.user)
     
+
+    function formatDuration(seconds) {
+        const minutes = Math.floor(Math.floor(seconds) / 60);
+        const remainingSeconds = seconds % 60;
+      
+        const formattedMinutes = String(minutes).padStart(2, '0');
+        const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+      
+        return `${formattedMinutes}:${formattedSeconds}`;
+      }
   
     useEffect(()=> {
       dispatch({type: isLoading.type ,payload:{ page: 1 }})
@@ -29,10 +40,11 @@ export default function Table() {
         dispatch(setMusicSlice(musicParam))
         setEditorActive(!editorActive)
     }
+
     const currentPage = (prev, next ) => prev ? prev + 1 : next ? next - 1 : 1;
   return (
     <>
-    
+
     {editorActive && <Editor editorActive={editorActive} setEditorActive={setEditorActive}/>}
     <Flex css={css`max-width:960px;margin: 0 auto;flex-direction:column`}>
     
@@ -52,13 +64,16 @@ export default function Table() {
             { musicList.loading ? <Skeleton/>:
             musicList.data.map((music,index) => (
 
-                <li key={music.id} className="music-list--item">
+                <li key={music._id} className="music-list--item" >
                     <Text>{((currentPage(musicList.prev, musicList.next) - 1) * 10)+(index+1)}</Text>
                     <Text className="text">{music.title}</Text>
                     <Text className="text artist">{music.artist}</Text>
-                    <Text className="text genre">{music.genre}</Text>
-                    <Text >{music.duration}</Text>
+                    <Text className="text genre">{music.genre.name}</Text>
+                    <Text >{ formatDuration(music.duration) }</Text>
                     <Flex css={css`gap: 5px;`}>
+                        <a target="_blank" style={{marginRight: '5px'}} href={music.path}><FontAwesomeIcon icon={faDownload}/></a>
+                        {music.owner == user._id &&  
+                        <>
                         <Button css={css`padding: 5px 10px;background-color: var(--link-color);cursor: pointer;
                         
                         &:hover {
@@ -69,14 +84,18 @@ export default function Table() {
                         <Button css={css`padding: 5px 10px; background-color: var(--link-danger);cursor: pointer;
                         &:hover {
                             background-color: var(--link-danger-light);
-                        }`} onClick={()=>dispatch({type: DELETE_MUSIC_BY_ID, payload: music.id})}>Delete</Button>
+                        }`} onClick={()=>dispatch({type: DELETE_MUSIC_BY_ID, payload: music._id})}>Delete</Button>
+                        </>
+                        }
+                        
                     </Flex>
                     
                 </li>
             ))
                     }
         </ul>
-        <Pagination/>
+        {(musicList.next || musicList.prev) && <Pagination/>}
+        
     </Flex>
     </>
   )
