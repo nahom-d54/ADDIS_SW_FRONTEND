02dc80cef1842eb62445e58b10efdddd5a9b7b7d
Nahom_d54 <nahom.d54@gmail.com>
2024-03-13 14:40:37 +0300

    feat: added code to pre fetch some data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { useDispatch } from 'react-redux';
 import './App.css'
 
 import Header from './components/Header';
 import Table from './components/Table';
+import { types } from './redux/sagas/userSaga';
+import { useEffect } from 'react';
+import { SET_GENRES } from './redux/sagas/types';
 
 
 function App() {
+
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch({type: SET_GENRES})
+    dispatch({type: types.RESTORE_SESSION})
+  },[dispatch])
+
+
  
   return (
     <>
