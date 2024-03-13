3931edd94562e8d0d4f9d4da8e2bc07b84a9ce3c
Nahom_d54 <nahom.d54@gmail.com>
2024-03-13 14:36:57 +0300

    fix: fixed some problems

--- src/redux/sagas/musicSaga.js
+++ src/redux/sagas/musicSaga.js
@@ -1,8 +1,9 @@
 import { startMusicsSlice,addMusicSlice, editMusicSlice, deleteMusicSlice, error, isLoading } from "../slices/musics";
-import { getMusicsAPI, getMusicByIdAPI,createMusicAPI, updateMusicByIdAPI, deleteMusicByIdAPI } from "../../apis"
+import { getMusicsAPI, getMusicByIdAPI,createMusicAPI, updateMusicByIdAPI, deleteMusicByIdAPI, getGenresAPI } from "../../apis"
 import { put, takeEvery } from 'redux-saga/effects'
 import { setMusicSlice } from "../slices/music";
-import { GET_MUSIC_BY_ID, CREATE_MUSIC, UPDATE_MUSIC_BY_ID, DELETE_MUSIC_BY_ID } from "./types";
+import { GET_MUSIC_BY_ID, CREATE_MUSIC, UPDATE_MUSIC_BY_ID, DELETE_MUSIC_BY_ID, SET_GENRES } from "./types";
+import { setGenresSlice } from "../slices/genres";
 
 // todo add try catch
 // error handling
@@ -19,18 +20,23 @@ export function* getMusicByIdSaga( action ) {
     yield put(setMusicSlice(music.data))
 }
 export function* createMusicSaga(action){
-    let modified =  action.payload
-    delete modified.id
-    const createMusic = yield createMusicAPI(modified)
+    
+    const createMusic = yield createMusicAPI(action.payload)
     yield put(addMusicSlice(createMusic.data))
 }
 export function* updateMusicSaga(action){
+    
     const updateMusic = yield updateMusicByIdAPI(action.payload)
     yield put(editMusicSlice(updateMusic.data))
 }
 export function* deleteMusicSaga(action){
     const deleteMusic = yield deleteMusicByIdAPI(action.payload)
     yield put(deleteMusicSlice(deleteMusic.data))
+}
+
+export function* setGenresSaga() {
+    const getGenres = yield getGenresAPI()
+    yield put(setGenresSlice(getGenres.data))
 }
 
 
@@ -42,5 +48,7 @@ export function* watchMusicAsync(){
     yield takeEvery(CREATE_MUSIC,createMusicSaga)
     yield takeEvery(UPDATE_MUSIC_BY_ID,updateMusicSaga)
     yield takeEvery(DELETE_MUSIC_BY_ID,deleteMusicSaga)
+    yield takeEvery(SET_GENRES,setGenresSaga)
+    
 }
 
