0f8228e341058326fd26066091ce8c5a2eab8d3a
Nahom_d54 <nahom.d54@gmail.com>
2024-03-13 14:37:51 +0300

    fix: fixed header state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,13 +5,22 @@ import Editor from "./Editor";
 import AboutPage from "./AboutPage";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch, useSelector } from "react-redux";
+import LoginForm from "./LoginForm";
+import RegistrationForm from "./RegistrationForm";
+import { types } from "../redux/sagas/userSaga";
 
 
 
 export default function Header() {
     const [editorActive, setEditorActive] = useState(false);
+    const [loginFormActive, setLoginFormActive] = useState(false);
+    const [registerFormActive, setRegisterFormActive] = useState(false);
     const [aboutPageActive, setAboutPageActive] = useState(false);
     const [isOpen, setIsOpen] = useState(false);
+    
+    const dispatch = useDispatch()
+    const user = useSelector(state => state.user)
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -20,7 +29,11 @@ export default function Header() {
   return (
     <>
     {aboutPageActive && <AboutPage aboutPageActive={aboutPageActive} setAboutPageActive={setAboutPageActive}/>}
-    {editorActive && <Editor editorActive={editorActive} setEditorActive={setEditorActive}/>}
+    {editorActive && <Editor editorActive={editorActive} setEditorActive={setEditorActive} type={'add'}/>}
+    
+
+    {loginFormActive && <LoginForm loginFormActive={loginFormActive} setLoginFormActive={setLoginFormActive}/>}
+    {registerFormActive && <RegistrationForm registerFormActive={registerFormActive} setRegisterFormActive={setRegisterFormActive}/>}
     <header>
       <Flex css={css`padding:1rem 3rem;justify-content: space-between`}>
         <Text className='brand' css={css`
@@ -36,17 +49,19 @@ export default function Header() {
           <Box className="humburgermenu" onClick={toggleMenu}><FontAwesomeIcon icon={faBars} size="2xl"/></Box>
           <Flex className={`menu ${isOpen ? 'open': ''}`}>
 
+            {user.loggedIn && 
             <Button backgroundColor="var(--link-color);cursor: pointer;
             &:hover {
               background-color: var(--link-color-light);
               }"
-              onClick={() => setEditorActive(!editorActive)}>Add Music</Button>
-            
+              onClick={() => setEditorActive(!editorActive)}>Add Music
+              </Button>
+            }
             <Button css={css`background: none;
             border-bottom: 1px solid #fff;
             padding:3px 0px;
             border-radius:0;
-            margin: 0 2rem;
+            margin: 0 .7rem;
             cursor: pointer;
             &:hover {
               border-color: #999;
@@ -54,6 +69,48 @@ export default function Header() {
             }`}
             
             onClick={()=> setAboutPageActive(!aboutPageActive)}>About</Button>
+            <Flex>
+            { user.loggedIn ?  (<Button css={css`background: none;
+            border-bottom: 1px solid #fff;
+            padding:3px 0px;
+            border-radius:0;
+            margin: 0 0.7rem;
+            cursor: pointer;
+            &:hover {
+              border-color: #999;
+              color: #999;
+            }`}
+            
+            onClick={() => dispatch({type: types.LOGOUT_USER})}>
+              Logout</Button>): 
+            (<><Button css={css`background: none;
+            border-bottom: 1px solid #fff;
+            padding:3px 0px;
+            border-radius:0;
+            margin: 0 .7rem;
+            cursor: pointer;
+            &:hover {
+              border-color: #999;
+              color: #999;
+            }`}
+            
+            onClick={()=> setLoginFormActive(!loginFormActive)}>
+              Login</Button>
+            <Button css={css`background: none;
+            border-bottom: 1px solid #fff;
+            padding:3px 0px;
+            border-radius:0;
+            margin: 0 .7rem;
+            cursor: pointer;
+            &:hover {
+              border-color: #999;
+              color: #999;
+            }`}
+            
+            onClick={()=> setRegisterFormActive(!registerFormActive)}>
+              Register</Button></>)
+              }
+            </Flex>
            </Flex>
            
         </nav>
